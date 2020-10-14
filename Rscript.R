c570b5b9d569ebc30ea09509293e508d3e9d00a9
Youngsang Kwon <ykwon@memphis.edu>
2020-10-14 13:20:01 -0500

    fourth

--- Rscript.R
+++ Rscript.R
@@ -1,10 +1,14 @@
 library(ggplot2)
 library(dplyr)
 library(sf)
+library(sp)
 
 data(mtcars)
 data(iris)
 head(mtcars)
 head(iris)
 
-ggplot(mtcars) + geom_point(aes(x = mpg, y = hp))
+ggplot(mtcars) + geom_point(aes(x = mpg, y = hp), color = "red", size = 3)
+
+
+ggplot(mtcars) + geom_point(aes(x = mpg, y = hp), color = "red", size = 3)
